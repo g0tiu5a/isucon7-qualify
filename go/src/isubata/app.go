16b9d8d3b8a96d3257c95482e377bb8175b06e75
Ryosuke Sato <rskjtwp@gmail.com>
2018-09-02 15:05:18 +0900

    Remove DELETE statement from initialize

--- go/src/isubata/app.go
+++ go/src/isubata/app.go
@@ -201,7 +201,6 @@ func register(name, password string) (int64, error) {
 
 func getInitialize(c echo.Context) error {
 	db.MustExec("DELETE FROM user WHERE id > 1000")
-	db.MustExec("DELETE FROM image WHERE id > 1001")
 	db.MustExec("DELETE FROM channel WHERE id > 10")
 	db.MustExec("DELETE FROM message WHERE id > 10000")
 	db.MustExec("DELETE FROM haveread")
